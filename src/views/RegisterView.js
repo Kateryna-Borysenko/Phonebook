298c8a400876e6015583dd2b1fb3244649d01324
Kateryna <k.borysenko.kyiv@gmail.com>
2022-02-12 12:15:00 +0200

    Added styles to RegisterView

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -3,13 +3,47 @@ import { useDispatch } from 'react-redux';
 import { authOperations } from '../redux/auth';
 
 const styles = {
+  wrap:{
+    margin: '0 auto'
+  },
+  title:{
+    fontFamily: 'inherit'
+  },
   form: {
-    width: 320,
+    width: 400,
   },
   label: {
     display: 'flex',
     flexDirection: 'column',
-    marginBottom: 15,
+    marginBottom: 20,
+  },
+  textField :{
+    marginTop: '10px',
+    width: '100%',
+    padding: '10px',
+    boxSizing: 'border-box',
+    fontFamily: 'inherit',
+    fontSize: '20px',
+    fontWeight: 200,
+    backgroundColor: 'inherit',
+    borderRadius: '5px',
+    border: '2px solid #007188',
+    cursor: 'pointer',
+    boxShadow: '0 1px 3px rgb(0 0 0 / 12%), 0 1px 1px rgb(0 0 0 / 14%)'
+  },
+  button: {
+    marginTop: '20px',
+    padding: '0 20px',
+    height: '50px',
+    fontSize: '16px',
+    lineHeight: '26px',
+    color: '#fff',
+    backgroundColor: '#007188',
+    borderRadius: '5px',
+    border: 'inherit',
+    boxSizing: 'border-box',
+    cursor: 'pointer',
+    boxShadow: '0 1px 3px rgb(0 0 0 / 12%), 0 1px 1px rgb(0 0 0 / 14%)'
   },
 };
 
@@ -41,36 +75,50 @@ export default function RegisterView() {
   };
 
   return (
-    <div>
-      <h1>Страница регистрации</h1>
+    <div style={styles.wrap}>
+      <h1 style={styles.title}>Страница регистрации</h1>
 
       <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
         <label style={styles.label}>
           Имя
-          <input type="text" name="name" value={name} onChange={handleChange} />
+          <input 
+          style={styles.textField}
+          type="text" 
+          name="name" 
+          value={name} 
+          onChange={handleChange} 
+          placeholder="John Doe"
+          required
+          />
         </label>
 
         <label style={styles.label}>
           Почта
           <input
+            style={styles.textField}
             type="email"
             name="email"
             value={email}
             onChange={handleChange}
+            placeholder="email@mail.com"
+            required
           />
         </label>
 
         <label style={styles.label}>
           Пароль
           <input
+            style={styles.textField}
             type="password"
             name="password"
             value={password}
             onChange={handleChange}
+            placeholder="qwerty1234"
+            required
           />
         </label>
 
-        <button type="submit">Зарегистрироваться</button>
+        <button style={styles.button} type="submit">Зарегистрироваться</button>
       </form>
     </div>
   );
